bc0e2c6b5303deb213c7adc94ed16ace44d66a13
adi1287 <30606961+adi1287@users.noreply.github.com>
2017-08-01 21:55:22 -0700

    Initial Commit

---
+++ PushToGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushToGit
+//
+//  Created by Aditya Gupta on 8/1/17.
+//  Copyright © 2017 Aditya Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
